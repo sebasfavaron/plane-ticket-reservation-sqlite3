b6450c0e8595b70bab404f20d2b4e5dcdae805c7
Sebas <sebas.favaron@hotmail.com>
2018-06-22 18:34:00 -0300

    muchas funciones para manejar querys de sql

--- server.c
+++ server.c
@@ -7,47 +7,151 @@
 #include <signal.h>
 #include <sqlite3.h>
 
+//gcc server.c -pthread -l sqlite3
+
+//Only one table called flights contains all the airport data
+//create table flights (flightName string, seatNumber int, isTaken int, primary key (flightName,seatNumber));
+
 char *TABLENAME = "flights";
 sqlite3 *airportDB;
+
+// Called once for every row returned by a sql query
+static int tableExists(void *boolean, int columnCount, char **data, char **columns) {
+   int *alreadyExists = boolean;
+   printf("tableExists\nBoolean before: %c\n",*alreadyExists);
+   for(int i=0; i<columnCount; i++){
+       printf("%s = %s\n",columns[i],data[i]);
+   }
+   if(strcmp(data[0], "0")) *alreadyExists = 1;
+   printf("Count result: %s\nBoolean after: %d\n\n",data[0],*alreadyExists);
+   return 0;
+}
+
 int createFlight(int seatAmount, char *flightName){
+    int isTaken = 0, rc, alreadyExists = 0;
+    char *errorMessage;    
+    char existsSQL[100];
     
-    char isTaken = '0';
-    char *errorMessage;
-    int rc;
+    snprintf(existsSQL,sizeof(existsSQL),"select count(flightName) from %s where flightName=\"%s\"",TABLENAME,flightName);
+    rc = sqlite3_exec(airportDB, existsSQL, tableExists, (void*)&alreadyExists, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "SQL query error: %s\n\n", errorMessage);
+        sqlite3_free(errorMessage);
+    }
+    if(alreadyExists || rc != SQLITE_OK) {
+        return -1;
+    }
 
     for(int currSeat=0; currSeat<seatAmount; currSeat++){
-        char *sql = "insert into ";
-        strcat(sql, TABLENAME);
-        strcat(sql, "values(");
-        strcat(sql, flightName);
-        strcat(sql, ",");
-        char seat[10];
-        itoa(currSeat, seat, 10);
-        strcat(sql, seat);
-        strcat(sql, ",");
-        strcat(sql, isTaken);
-        strcat(sql, ");");
+        char sql[100];
+        snprintf(sql, sizeof(sql), "insert into %s values(\"%s\",%d,%d)", TABLENAME, flightName, currSeat, isTaken);
+        printf("Command: %s\n",sql);
         rc = sqlite3_exec(airportDB, sql, NULL, 0, &errorMessage);
         if(rc != SQLITE_OK){
-            fprintf(stderr, "SQL insert error: %s\n", errorMessage);
+            fprintf(stderr, "SQL insert error: %s\n\n", errorMessage);
             sqlite3_free(errorMessage);
         } else {
-            printf("Value %i inserted correctly into table %s", currSeat, flightName);
+            printf("Value %i inserted correctly into table %s\n\n", currSeat, TABLENAME);
         }
+    }
+    return 0;
+}
+
+// No creo que se use nunca
+int clearTable(){
+    int rc;
+    char deleteSQL[100];
+    char *errorMessage;
+    
+    snprintf(deleteSQL,sizeof(deleteSQL),"delete from %s",TABLENAME);
+    rc = sqlite3_exec(airportDB, deleteSQL, NULL, NULL, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "SQL delete error: %s\n\n", errorMessage);
+        sqlite3_free(errorMessage);
+        return -1;
+    } else {
+        printf("Table %s deleted correctly\n\n", TABLENAME);
+        return 0;
     }
 }
 
-int main(int argc, char **argv){
-   int rc = sqlite3_open("airport.db", &airportDB);
+// funcion para debugging, pero no anda
+int customQuery(){
+    int rc;
+    char customSQL[100];
+    char *errorMessage;
+    
+    scanf("%s",customSQL);
+    rc = sqlite3_exec(airportDB, customSQL, NULL, NULL, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "SQL custom query error: %s\n\n", errorMessage);
+        sqlite3_free(errorMessage);
+        return -1;
+    } else {
+        printf("Custom query correctly executed\n\n");
+        return 0;
+    }
+}
 
-   if(rc) {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(airportDB));
-      return 0;
-   } else {
-      fprintf(stderr, "Opened database successfully\n");
+static int seatTaken(void *boolean, int columnCount, char **data, char **columns) {
+   int *taken = boolean;
+   printf("seatTaken\nBoolean before: %c\n",*taken);
+   for(int i=0; i<columnCount; i++){
+       printf("%s = %s\n",columns[i],data[i]);
    }
+   if(strcmp(data[0], "0")) *taken = 1;
+   printf("Count result: %s\nBoolean after: %d\n\n",data[0],*taken);
+   return 0;
+}
 
-    createFlight(4,"ACB132");
+int reserveSeat(char *flightName, int seatNumber){
+    int rc, isTaken = 0;
+    char takenQuery[100];
+    char *errorMessage;
+    
+    snprintf(takenQuery,sizeof(takenQuery),"select isTaken from %s where flightName=\"%s\" and seatNumber=\"%d\"", TABLENAME, flightName, seatNumber);
+    rc = sqlite3_exec(airportDB, takenQuery, seatTaken, (void*)&isTaken, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "SQL query error: %s\n\n", errorMessage);
+        sqlite3_free(errorMessage);
+    }
+    if(isTaken || rc != SQLITE_OK) {
+        return -1;
+    }
 
-   sqlite3_close(airportDB);
+    char query[100];
+    snprintf(query, sizeof(query), "update %s set isTaken=1 where flightName=\"%s\" and seatNumber=\"%d\"", TABLENAME, flightName, seatNumber);
+    rc = sqlite3_exec(airportDB, query, NULL, NULL, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "SQL query error: %s\n\n", errorMessage);
+        sqlite3_free(errorMessage);
+        return -1;
+    } else {
+        printf("Query correctly executed\n\n");
+        return 0;
+    }
+}
+
+void closeDatabase(){
+    sqlite3_close(airportDB);
+    printf("Closed database\n");
+}
+
+int main(int argc, char **argv){
+    //todo: falta crear airport.db si no existe y crear ahi la tabla flights con su clave
+    int rc = sqlite3_open("airport.db", &airportDB);
+
+    if(rc) {
+        fprintf(stderr, "Can't open database: %s\n\n", sqlite3_errmsg(airportDB));
+        return 0;
+    } else {
+        fprintf(stderr, "Opened database successfully\n\n");
+    }
+
+    //aca atiende a los clientes con forks y threads. Muchas funciones de sql creadas aca
+    //van a tener que moverse a un requestHandler.c a donde mandemos a cada cliente
+    createFlight(10,"ECO123");
+    reserveSeat("ECO123", 2);
+
+    closeDatabase();
 }
