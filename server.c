b2ea828d2755c3c173d2933d65128c8246da822c
Sebas <sebas.favaron@hotmail.com>
2018-06-23 01:36:23 -0300

    sqlite all commands

--- server.c
+++ server.c
@@ -18,12 +18,12 @@ sqlite3 *airportDB;
 // Called once for every row returned by a sql query
 static int tableExists(void *boolean, int columnCount, char **data, char **columns) {
    int *alreadyExists = boolean;
-   printf("tableExists\nBoolean before: %c\n",*alreadyExists);
+   //printf("tableExists\nBoolean before: %d\n",*alreadyExists);
    for(int i=0; i<columnCount; i++){
-       printf("%s = %s\n",columns[i],data[i]);
+       //printf("%s = %s\n",columns[i],data[i]);
    }
    if(strcmp(data[0], "0")) *alreadyExists = 1;
-   printf("Count result: %s\nBoolean after: %d\n\n",data[0],*alreadyExists);
+   //printf("Count result: %s\nBoolean after: %d\n\n",data[0],*alreadyExists);
    return 0;
 }
 
@@ -37,24 +37,43 @@ int createFlight(int seatAmount, char *flightName){
     if(rc != SQLITE_OK){
         fprintf(stderr, "SQL query error: %s\n\n", errorMessage);
         sqlite3_free(errorMessage);
+        return -1;
     }
-    if(alreadyExists || rc != SQLITE_OK) {
+    if(alreadyExists) {
+        printf("Flight %s already exists\n\n", flightName);
         return -1;
     }
 
     for(int currSeat=0; currSeat<seatAmount; currSeat++){
         char sql[100];
-        snprintf(sql, sizeof(sql), "insert into %s values(\"%s\",%d,%d)", TABLENAME, flightName, currSeat, isTaken);
-        printf("Command: %s\n",sql);
+        snprintf(sql, sizeof(sql), "insert into %s values(\"%s\",%d,%d)", TABLENAME, flightName, currSeat+1, isTaken);
+        //printf("Command: %s\n",sql);
         rc = sqlite3_exec(airportDB, sql, NULL, 0, &errorMessage);
         if(rc != SQLITE_OK){
             fprintf(stderr, "SQL insert error: %s\n\n", errorMessage);
             sqlite3_free(errorMessage);
         } else {
-            printf("Value %i inserted correctly into table %s\n\n", currSeat, TABLENAME);
+            printf("Value %i inserted correctly into table %s, in flight %s\n", currSeat, TABLENAME, flightName);
         }
     }
     return 0;
+}
+
+int cancelFlight(char *flightName){
+    int rc;
+    char deleteSQL[100];
+    char *errorMessage;
+    
+    snprintf(deleteSQL,sizeof(deleteSQL),"delete from %s where flightName=\"%s\"",TABLENAME, flightName);
+    rc = sqlite3_exec(airportDB, deleteSQL, NULL, NULL, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "SQL delete error: %s\n\n", errorMessage);
+        sqlite3_free(errorMessage);
+        return -1;
+    } else {
+        printf("Flight %s deleted correctly\n\n", flightName);
+        return 0;
+    }
 }
 
 // No creo que se use nunca
@@ -95,12 +114,12 @@ int customQuery(){
 
 static int seatTaken(void *boolean, int columnCount, char **data, char **columns) {
    int *taken = boolean;
-   printf("seatTaken\nBoolean before: %c\n",*taken);
+   //printf("seatTaken\nBoolean before: %c\n",*taken);
    for(int i=0; i<columnCount; i++){
        printf("%s = %s\n",columns[i],data[i]);
    }
    if(strcmp(data[0], "0")) *taken = 1;
-   printf("Count result: %s\nBoolean after: %d\n\n",data[0],*taken);
+   //printf("Count result: %s\nBoolean after: %d\n\n",data[0],*taken);
    return 0;
 }
 
@@ -114,8 +133,10 @@ int reserveSeat(char *flightName, int seatNumber){
     if(rc != SQLITE_OK){
         fprintf(stderr, "SQL query error: %s\n\n", errorMessage);
         sqlite3_free(errorMessage);
+        return -1;
     }
-    if(isTaken || rc != SQLITE_OK) {
+    if(isTaken) {
+        printf("Seat %d in flight %s already taken\n\n", seatNumber, flightName);
         return -1;
     }
 
@@ -127,7 +148,37 @@ int reserveSeat(char *flightName, int seatNumber){
         sqlite3_free(errorMessage);
         return -1;
     } else {
-        printf("Query correctly executed\n\n");
+        printf("Seat %d in flight %s correctly reserved\n\n", seatNumber, flightName);
+        return 0;
+    }
+}
+
+int cancelSeat(char *flightName, int seatNumber){
+    int rc, isTaken = 1;
+    char cancelQuery[100];
+    char *errorMessage;
+    
+    snprintf(cancelQuery,sizeof(cancelQuery),"select isTaken from %s where flightName=\"%s\" and seatNumber=\"%d\"", TABLENAME, flightName, seatNumber);
+    rc = sqlite3_exec(airportDB, cancelQuery, seatTaken, (void*)&isTaken, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "SQL query error: %s\n\n", errorMessage);
+        sqlite3_free(errorMessage);
+        return -1;
+    }
+    if(!isTaken) {
+        printf("Seat %d in flight %s is not taken, so it can't be canceled\n\n", seatNumber, flightName);
+        return -1;
+    }
+
+    char query[100];
+    snprintf(query, sizeof(query), "update %s set isTaken=0 where flightName=\"%s\" and seatNumber=\"%d\"", TABLENAME, flightName, seatNumber);
+    rc = sqlite3_exec(airportDB, query, NULL, NULL, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "SQL query error: %s\n\n", errorMessage);
+        sqlite3_free(errorMessage);
+        return -1;
+    } else {
+        printf("Seat %d in flight %s correctly canceled\n\n", seatNumber, flightName);
         return 0;
     }
 }
@@ -138,20 +189,29 @@ void closeDatabase(){
 }
 
 int main(int argc, char **argv){
-    //todo: falta crear airport.db si no existe y crear ahi la tabla flights con su clave
+    char *errorMessage;
     int rc = sqlite3_open("airport.db", &airportDB);
-
     if(rc) {
-        fprintf(stderr, "Can't open database: %s\n\n", sqlite3_errmsg(airportDB));
+        fprintf(stderr, "\nCan't open database: %s\n\n", sqlite3_errmsg(airportDB));
         return 0;
     } else {
-        fprintf(stderr, "Opened database successfully\n\n");
+        fprintf(stderr, "\nOpened database successfully\n\n");
+    }
+    //crear tabla flights si no existe
+    char createFlights[256] = "create table if not exists flights (flightName string, seatNumber int, isTaken int, primary key (flightName,seatNumber))";
+    rc = sqlite3_exec(airportDB, createFlights, NULL, NULL, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "SQL query error: %s\n", errorMessage);
+        sqlite3_free(errorMessage);
+        return -1;
+    } else {
+        printf("Table \"flights\" correctly initialized/created\n\n");
     }
 
     //aca atiende a los clientes con forks y threads. Muchas funciones de sql creadas aca
     //van a tener que moverse a un requestHandler.c a donde mandemos a cada cliente
-    createFlight(10,"ECO123");
-    reserveSeat("ECO123", 2);
+    
+    //si los querys estan bien no tira error, pero no avisa que esta mal
 
     closeDatabase();
 }
